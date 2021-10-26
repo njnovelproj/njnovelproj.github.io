fe54ca71c3156cf997c4b8e76bc830896415ba59
Niño James A. Tan <njtan142@gmail.com>
2021-10-26 15:14:23 +0800

    final yet unresponsive

--- soul_land_2/app.js
+++ soul_land_2/app.js
@@ -1,9 +1,9 @@
 
 var body = document.body,
-    html = document.documentElement;
+html = document.documentElement;
 
 var height = Math.max(body.scrollHeight, body.offsetHeight,
-    html.clientHeight, html.scrollHeight, html.offsetHeight);
+html.clientHeight, html.scrollHeight, html.offsetHeight);
 var canvas = document.getElementById("myCanvas");
 canvas.width = document.body.clientWidth; //document.width is obsolete
 canvas.height = height; //document.height is obsolete
@@ -14,22 +14,41 @@ grd.addColorStop(1, "black");
 
 
 function drawBall() {
-    var x = Math.floor(Math.random() * document.body.clientWidth);
-    var y = Math.floor(Math.random() * height);
-    ctx.beginPath();
-    ctx.arc(x, y, 1, 0, 2 * Math.PI);
-    ctx.fillStyle = "#FFFFFF96";
-    ctx.fill();
-    ctx.closePath();
+var x = Math.floor(Math.random() * document.body.clientWidth);
+var y = Math.floor(Math.random() * height);
+ctx.beginPath();
+ctx.arc(x, y, 1, 0, 2 * Math.PI);
+ctx.fillStyle = "#FFFFFF96";
+ctx.fill();
+ctx.closePath();
 }
 
 function draw() {
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < 1000; i++) {
-        drawBall();
-    }
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+for (let i = 0; i < 1000; i++) {
+    drawBall();
+}
 
 }
 // draw()
-  setInterval(draw, 1000);
+setInterval(draw, 1000);
+
+var is_changed = false
+function change(){
+var parent = document.getElementById("description")
+var btn = document.getElementById("btn")
+if(!is_changed){
+    btn.innerHTML = "<"
+    parent.children[0].innerHTML = "History"
+    parent.children[2].innerHTML = `Huo Yuhao was born as the son of the Duke of the White Tiger Clan and his maid. Following the Duke's departure to the on-going war, the Duchess took over the reins of the manor and, being jealous and seeing Huo Yuhao as a threat to her children, secluded him and his mother in a shack making up stories that she contracted an infectious disease. This plus the malnourishment caused Huo Yuhao's mother, who already had a weak constitution, to fall sick and die when Huo Yuhao was 10 years old. He vowed to become a great Spirit Master and take revenge.
+
+    At the age of six, he awakened his spirit, an extremely rare variant body spirit known as Spirit Eyes. However, at the time of awakening, his Spirit Power was only at rank 1 and it took him 5 years to get to rank 10 which is 2 years more than what people usually take.`
+
+}else{
+    btn.innerHTML = ">"
+    parent.children[0].innerHTML = "Personality"
+    parent.children[2].innerHTML = `Huo Yuhao is a kind person, not hesitating to help others in need. He has a strong determination and works very hard towards achieving his goals. He is a bit distrustful of others since his past experiences. Sadly despite his kind nature, due to his past, he has become narrow-minded and filled with an inner hatred that blinds him if it is related to his past.`
+}
+is_changed = !is_changed
+}
--- soul_land_3/app.js
+++ soul_land_3/app.js
@@ -1,9 +1,9 @@
 
 var body = document.body,
-    html = document.documentElement;
+html = document.documentElement;
 
 var height = Math.max(body.scrollHeight, body.offsetHeight,
-    html.clientHeight, html.scrollHeight, html.offsetHeight);
+html.clientHeight, html.scrollHeight, html.offsetHeight);
 var canvas = document.getElementById("myCanvas");
 canvas.width = document.body.clientWidth; //document.width is obsolete
 canvas.height = height; //document.height is obsolete
@@ -14,22 +14,43 @@ grd.addColorStop(1, "black");
 
 
 function drawBall() {
-    var x = Math.floor(Math.random() * document.body.clientWidth);
-    var y = Math.floor(Math.random() * height);
-    ctx.beginPath();
-    ctx.arc(x, y, 1, 0, 2 * Math.PI);
-    ctx.fillStyle = "#FFFFFF96";
-    ctx.fill();
-    ctx.closePath();
+var x = Math.floor(Math.random() * document.body.clientWidth);
+var y = Math.floor(Math.random() * height);
+ctx.beginPath();
+ctx.arc(x, y, 1, 0, 2 * Math.PI);
+ctx.fillStyle = "#FFFFFF96";
+ctx.fill();
+ctx.closePath();
 }
 
 function draw() {
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < 1000; i++) {
-        drawBall();
-    }
+ctx.fillStyle = grd;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+for (let i = 0; i < 1000; i++) {
+    drawBall();
+}
 
 }
 // draw()
-  setInterval(draw, 1000);
+setInterval(draw, 1000);
+
+var is_changed = false
+function change(){
+var parent = document.getElementById("description")
+var btn = document.getElementById("btn")
+if(!is_changed){
+    btn.innerHTML = "<"
+    parent.children[0].innerHTML = "Personality"
+    parent.children[2].innerHTML = `In Soul Land 4, due to his amnesia, Wulin acts rather stoic. He talks very little and regularly has an emotionless expression. Still, he retains his kind heart, being grateful to Le Qingling for taking him in.
+
+    But in Soul Land 4 volume 14, when he met with Gu Yuena, his heart was open for her again, and he turned back to the outgoing man.
+    
+    In Soul Land 4 volume 16, after he and Xuanyu both knew each other's father-son relationship, he became quite stable and mature.`
+
+}else{
+    btn.innerHTML = ">"
+    parent.children[0].innerHTML = "Personality"
+    parent.children[2].innerHTML = `Tang Wulin is an honest and kindhearted individual, which is apparent when he saved Na'Er from a group of teenagers, who were trying to kidnap her and sell her off. He is incredibly hardworking, not letting the words of others affect him negatively. Also having been raised in an impoverished family, he is significantly more sensible and mature than others of his age.`
+}
+is_changed = !is_changed
+}
